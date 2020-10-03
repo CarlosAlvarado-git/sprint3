cb153ff55c02e30ccae5dc29d7614888e5e02773
Carlos Alvarado <cmalvarado@ufm.edu>
2020-10-02 18:59:50 -0600

    Create CambiarPos.cs

---
+++ GarbageCollectorSprint3/CambiarPos.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarPos : MonoBehaviour
+{
+    private Vector3 cambio;
+    private float y = 1.0f;
+    private float m_Speed = 2.0f;
+    public Rigidbody cuboquecambia;
+    void Start()
+    {
+        cuboquecambia = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        GC();   
+    }
+    void GC(){   
+        if(y < 5.0f){
+            y = y + 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y + 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y + 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y - 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y + 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y + 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y - 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y + 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            y = y - 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            cuboquecambia.position = cambio * m_Speed;  
+            GC();
+        }
+        else{
+            y = 1.0f;
+            cambio = new Vector3(1.0f, y, 1.0f);
+            cuboquecambia.position = cambio * m_Speed;
+            System.GC.Collect();
+        }        
+    }
+}
